694268dfc6e136357992554be4036ba0bc5334aa
John Farina <john.farina0824@gmail.com>
2019-02-14 21:18:09 -0500

    Minor refactoring to methods in game_functions

--- game_functions.py
+++ game_functions.py
@@ -1,23 +1,33 @@
 import sys
 import pygame
 
+
+def check_keydown_events(event, ship):
+    """Responds to key presses"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Responds to key releases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Responsible for handling key and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
 
 def update_screen(invasion_settings, screen, ship):
     """Responsible for updating images on screen"""
